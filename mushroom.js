2ae3ba180380d64b16824b07712e6a60a4acf384
phoetius <phoetius@gmail.com>
2017-01-28 14:01:12 -0500

    Added fade in of mushrooms with delay

--- mushroom.js
+++ mushroom.js
@@ -10,17 +10,25 @@ function Mushroom(x,y)
     this.bdis = dis(garden.x, garden.y, this.x, this.y);
     this.bdir = dir(garden.x, garden.y, this.x, this.y);
     this.scale = Math.random()*.5+.5;
+    this.delay = Math.random()*2;
     
     this.install = function()
     {
         this.element = document.createElement("img");
 
         var i = Math.floor(Math.random()*4);
-        var imgs = ["shroomBox70.svg", "shroomBox137.svg", "shroomBox171.svg", "shroomBox229.svg"]
-        this.element.setAttribute("src", imgs[i])
+        var imgs = ["shroomBox70.svg", "shroomBox137.svg", "shroomBox171.svg", "shroomBox229.svg"];
+        this.element.setAttribute("src", imgs[i]);
         this.element.style.position = "fixed";
         this.element.style.zIndex = 100;
-    
+        
+        this.element.style.opacity = "0";
+        this.element.style.animationFillMode = "forwards";
+        this.element.style.animationName = "fadein";
+        this.element.style.animationDuration = "1s";
+        this.element.style.animationDelay = this.delay.toString() + "s";
+        
+        
         document.body.appendChild(this.element);
 
         this.element.style.transform = "scale("+this.scale+","+this.scale+")";
@@ -42,8 +50,6 @@ function Mushroom(x,y)
     {
         this.x = garden.x+ldx(this.bdis, this.bdir+garden.rot);
         this.y = garden.y+ldy(this.bdis*garden.tilt, this.bdir+garden.rot);
-
-        console.log(this.x)
         
         //this.element.style.left = (this.x-12) + "px";
         //this.element.style.top = (this.y-35) + "px";
